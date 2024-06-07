9407c49f30e9955125a505f9c7aa88c234d83b2b
Jaylynn <jmiri697@gmail.com>
2024-06-07 12:17:00 -0400

    mierda

--- main code stuff/duel.py
+++ main code stuff/duel.py
@@ -2,10 +2,27 @@ import random
 from flask import Flask, redirect, request 
 
 game = Flask(__name__)
-@game.route('/duel', methods=['GET', 'POST']):
+@game.route('/duel', methods=['GET', 'POST'])
 def duel_numero_uno():
     answers = ['left', 'right', 'center']
 
     if request.method == 'POST':
         selected = request.form['selected']
+        if selected == answers[0]:
+            return redirect('/morir')
+        if selected == answers[1]:
+            return redirect('/mierda')
+        if selected == answers[2]:
+            return redirect('/ganar')
         
+@game.route('/morir', methods=['GET', 'POST'])
+def lose():
+    return ''
+
+@game.route('/mierda', methods=['GET', 'POST'])
+def tie():
+    return ''
+
+@game.route('/ganar', methods=['GET', 'POST'])
+def win():
+    return ''
