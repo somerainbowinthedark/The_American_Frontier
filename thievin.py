2d8c0469ecd40a3f16dcfb504c2be98b0bec805c
cmorales <caitlyn.morales424@gmail.com>
2024-06-10 14:38:08 -0400

    yep

--- thievin.py
+++ thievin.py
@@ -1,9 +1,7 @@
 from flask import Flask, render_template, request
 from titles import app
 from titles import character_select
-import random
-
 
 @app.route('/thievery', methods =['GET', 'POST'])
 def theives():
-    return 'OK'
+    r
