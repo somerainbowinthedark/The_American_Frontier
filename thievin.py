2b8da79987a7adf0b90b79bb8f06117a46cbae5f
cmorales <caitlyn.morales424@gmail.com>
2024-06-11 08:21:23 -0400

    yip

--- thievin.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask, render_template, request
-from titles import app
-from titles import character_select
-
-@app.route('/thievery', methods =['GET', 'POST'])
-def theives():
-    r
